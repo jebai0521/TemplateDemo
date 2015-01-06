c1c19e17406298d7959cd8146806fa5fb1972027
jebai <jebai@qq.com>
2015-01-07 02:37:56 +0800

    update

--- app/controllers/render.js
+++ app/controllers/render.js
@@ -6,23 +6,12 @@ function _onBack(e) {
     $.container.close();
 }
 
-
 var path = Ti.Filesystem.getFile(Ti.Filesystem.getResourcesDirectory(), '/templates/index.html');
 
-Ti.API.info(path.nativePath);
-
-// $.webview.setUrl(path.nativePath);
-
-// $.webview.setHtml(path.read().getText());
-
-    var ejs = require("ejs");
-
-    // Ti.API.info(ejs);
+var ejs = require("ejs");
     
-    var str = "<h1><%= name %></h1>";
-    
-    args.filename = path.resolve();
-    
-    var ret = ejs.render(path.read().getText(), args);  
-    
-    $.webview.setHtml(ret);
+args.filename = path.resolve();
+
+var ret = ejs.render(path.read().getText(), args);  
+
+$.webview.setHtml(ret);
